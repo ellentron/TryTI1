a20ee0dd982395c74fce498bfce6905c8039d10d
Danny E <danny@ellentron.com>
2023-02-07 22:03:51 +0200

    Small changes that didn't help

--- UiSections/uiSection2.py
+++ UiSections/uiSection2.py
@@ -15,23 +15,22 @@ class UiSect2(ttk.Frame):
         lbl_uut.grid(row=0, column=0, sticky="nsew", padx=(10, 0))
 
         # Create a frame for the 2nd row of section 2
-        frm_uut = ttk.Frame(self, padding="3 3 3 3")
-        frm_uut.grid(row=1, column=0, sticky="nsew")
+        frm_select_uut = ttk.Frame(self, padding="3 3 3 3")
+        frm_select_uut.grid(row=1, column=0, sticky="nsew")
 
         # in the 2nd row frame, create a grid layout that contains 1 row and 3 columns
-        frm_uut.grid_rowconfigure(0, weight=1)
-        frm_uut.grid_columnconfigure(0, weight=1)
-        frm_uut.grid_columnconfigure(1, weight=1)
-        frm_uut.grid_columnconfigure(2, weight=1)
+        frm_select_uut.grid_rowconfigure(0, weight=1)
+        frm_select_uut.grid_columnconfigure(0, weight=1)
+        frm_select_uut.grid_columnconfigure(1, weight=1)
+        frm_select_uut.grid_columnconfigure(2, weight=1)
 
-        # Create LabelFrame for the 1st column of the 2nd row frame
-        # frm_uut1 = ttk.LabelFrame(frm_uut, text="UUT 1", padding="3 3 0 3")
-        # frm_uut1 = ttk.LabelFrame(frm_uut, text="UUT 1", padding="3 3 0 3", labelanchor='n')
-        font_small = font.Font(size=10)
-        frm_uut1 = ttk.LabelFrame(frm_uut, text="UUT 1", padding="0 0 0 0")
-        frm_uut1.grid(row=0, column=0, sticky="nsew")
+        # Create LabelFrame as the container for Laser Selection radio buttons, in the 1st column of the 2nd row frame
+        font_small = font.Font(size=8)
+        frm_laser = ttk.LabelFrame(frm_select_uut, text="Select Laser", padding="0 0 0 0")
+        frm_laser.grid(row=0, column=0, sticky="nsew")
+        #frm_uut1 = ttk.LabelFrame(frm_select_uut, text="UUT 1", padding="0 0 0 0", font=font.Font(size=8))
+        #frm_laser.grid(row=0, column=0, sticky="nsew")
 
-        frm_uut1.grid(row=0, column=0, sticky="nsew")
 
 
 
